16eb1771b7b501d2fef722d36a1c984b5d4aa187
marc-enzo.bonnafon <marc-enzo.bonnafon@teads.tv>
2020-12-05 20:48:10 +0100

    Update day4 2019

--- 2019/day4/main.go
+++ 2019/day4/main.go
@@ -5,63 +5,68 @@ import (
 	"strconv"
 )
 
-func part1(lowerBoundary, upperBoundary int) int {
-	var counter int
-	for i := lowerBoundary; i <= upperBoundary; i++ {
-		istring := strconv.Itoa(i)
-		similiar := false
-		for j := 0; j < 5; j++ {
-			if istring[j:j+1] == istring[j+1:j+2] {
-				similiar = true
-			}
-			if istring[j:j+1] > istring[j+1:j+2] {
-				j = 5
-			} else if (j == 4) && (similiar) {
-				counter++
-			}
+func validatePassword(i int) bool {
+	var similar bool
+	s := strconv.Itoa(i)
+	for i := 0; i < len(s)-1; i++ {
+		if s[i] > s[i+1] {
+			return false
+		}
+		if s[i] == s[i+1] {
+			similar = true
 		}
 	}
-	return counter
+	return similar
 }
 
-// Part2 z
-func Part2(lowerBoundary, upperBoundary int) int {
-	var counter int
-	for i := lowerBoundary; i <= upperBoundary; i++ {
-		istring := strconv.Itoa(i)
-		mapOfNumbers := make(map[string]int)
-		similiar := false
-		for j := 0; j < 5; j++ {
-			val1 := istring[j : j+1]
-			val2 := istring[j+1 : j+2]
-
-			if val1 > val2 {
-				j = 5
-			}
-
-			if val1 == val2 {
-				mapOfNumbers[val1]++
-			} else if mapOfNumbers[val1]%2 != 0 {
-				delete(mapOfNumbers, val1)
-				similiar = true
-			}
-			if j == 4 {
-				if mapOfNumbers[val1]%2 != 0 {
-					delete(mapOfNumbers, val1)
-					similiar = true
-				}
-				if len(mapOfNumbers) == 0 && similiar {
-					counter++
-				}
-			}
-			//fmt.Println(j, "-", val1, val2, mapOfNumbers)
+func pt1(lowerBoundary, upperBoundary int) int {
+	var count int
+	for lowerBoundary <= upperBoundary {
+		if validatePassword(lowerBoundary) {
+			count++
 		}
-		//fmt.Println("_____________")
+		lowerBoundary++
 	}
-	return counter
+	return count
+}
+
+func validatePasswordPt2(i int) bool {
+	var similar bool
+	var count int
+	s := strconv.Itoa(i)
+	for i := 0; i < len(s)-1; i++ {
+		if s[i] > s[i+1] {
+			return false
+		}
+		if s[i] == s[i+1] {
+			if (i == len(s)-2) && similar && (count%2 == 1) {
+				return false
+			}
+			similar = true
+			count++
+		} else {
+			if similar && (count%2 == 0) {
+				return false
+			}
+			similar = false
+			count = 0
+		}
+	}
+	return true
+}
+
+func pt2(lowerBoundary, upperBoundary int) int {
+	var count int
+	for lowerBoundary <= upperBoundary {
+		if validatePasswordPt2(lowerBoundary) {
+			count++
+		}
+		lowerBoundary++
+	}
+	return count
 }
 
 func main() {
-	fmt.Println(part1(356261, 846303))
-	fmt.Println(Part2(356261, 846303))
+	fmt.Printf("Part 1. %d passwords within the range input meet criterias\n", pt1(356261, 846303))
+	fmt.Printf("Part 2. %d passwords within the range input meet criterias\n", pt2(356261, 846303))
 }
