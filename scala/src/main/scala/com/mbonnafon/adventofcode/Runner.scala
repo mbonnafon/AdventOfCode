d79565502d31e291a3321274c72722bb50ddb523
marc-enzo.bonnafon <marcenzo.bonnafon@gmail.com>
2021-12-03 00:11:50 +0100

    [scala] add day2 2021

--- scala/src/main/scala/com/mbonnafon/adventofcode/Runner.scala
+++ scala/src/main/scala/com/mbonnafon/adventofcode/Runner.scala
@@ -4,7 +4,10 @@ object Runner extends App {
   val year = args.headOption.map(_.toInt).getOrElse(2021)
   val day = args.lift(1).map(_.toInt).getOrElse(1)
 
-  def puzzleMap = Map((2021, 1) -> year2021.Day01)
+  def puzzleMap = Map(
+    (2021, 1) -> year2021.Day01,
+    (2021, 2) -> year2021.Day02
+  )
 
   puzzleMap.get(year, day) match {
     case None => println(s"Puzzle for Day $day (Year $year) is not yet solved!")
--- scala/src/main/scala/com/mbonnafon/adventofcode/year2021/Day02.scala
+++ scala/src/main/scala/com/mbonnafon/adventofcode/year2021/Day02.scala
@@ -4,8 +4,33 @@ import com.mbonnafon.adventofcode.Puzzle
 
 object Day02 extends Puzzle {
 
-  def part1(input: List[String]): Int = 0
+  def part1(input: List[String]): Int = {
+    val (horizontal, depth) = {
+      input
+        .foldLeft((0, 0)) { case ((horizontal, depth), command) =>
+          command match {
+            case s"forward $unit" => (horizontal + unit.toInt, depth)
+            case s"down $unit"    => (horizontal, depth + unit.toInt)
+            case s"up $unit"      => (horizontal, depth - unit.toInt)
+          }
+        }
+    }
+    horizontal * depth
+  }
 
-  def part2(input: List[String]): Int = 0
+  def part2(input: List[String]): Int = {
+    val (horizontal, depth, _) = {
+      input
+        .foldLeft((0, 0, 0)) { case ((horizontal, depth, aim), command) =>
+          command match {
+            case s"down $unit" => (horizontal, depth, aim + unit.toInt)
+            case s"up $unit"   => (horizontal, depth, aim - unit.toInt)
+            case s"forward $unit" =>
+              (horizontal + unit.toInt, depth + aim * unit.toInt, aim)
+          }
+        }
+    }
+    horizontal * depth
+  }
 
 }
