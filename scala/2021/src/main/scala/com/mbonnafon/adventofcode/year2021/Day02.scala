c138d389ab480cb8b355ea46582c0c5572111bb4
marc-enzo.bonnafon <marcenzo.bonnafon@gmail.com>
2021-12-02 23:37:27 +0100

    [scala] bootstrap day2 2021

---
+++ scala/2021/src/main/scala/com/mbonnafon/adventofcode/year2021/Day02.scala
@@ -0,0 +1,11 @@
+package com.mbonnafon.adventofcode.year2021
+
+import com.mbonnafon.adventofcode.Puzzle
+
+object Day02 extends Puzzle {
+
+  def part1(input: List[Int]): Int = 0
+
+  def part2(input: List[Int]): Int = 0
+
+}
