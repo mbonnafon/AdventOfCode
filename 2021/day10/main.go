86e0be81bf15c8a2151dcdb9115d5c022b827bee
marc-enzo.bonnafon <marcenzo.bonnafon@gmail.com>
2021-12-10 14:41:19 +0100

    [go] add day 10 2021

---
+++ 2021/day10/main.go
@@ -0,0 +1,114 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+
+	"github.com/mbonnafon/AdventOfCode/helpers"
+)
+
+func main() {
+	lines, _ := helpers.StringLines("./input.txt")
+	fmt.Println("Part 1. :", pt1(lines))
+	fmt.Println("Part 2. :", pt2(lines))
+}
+
+func pt1(lines []string) int {
+	var score int
+	for _, l := range lines {
+		switch processCorruptedLine(l) {
+		case ")":
+			score += 3
+		case "]":
+			score += 57
+		case "}":
+			score += 1197
+		case ">":
+			score += 25137
+		}
+	}
+	return score
+}
+
+func pt2(lines []string) int {
+	var incompleteLines []string
+	for _, l := range lines {
+		if processCorruptedLine(l) == "" {
+			incompleteLines = append(incompleteLines, l)
+		}
+	}
+
+	var scores []int
+	for _, l := range incompleteLines {
+		scores = append(scores, scoreIncompleteLine(l))
+	}
+	sort.Ints(scores)
+
+	return scores[len(scores)/2]
+}
+
+func processCorruptedLine(l string) string {
+	var stack helpers.Stack
+	for _, c := range l {
+		symbol := string(c)
+		switch symbol {
+		case "(":
+			stack.Push(symbol)
+		case "{":
+			stack.Push(symbol)
+		case "[":
+			stack.Push(symbol)
+		case "<":
+			stack.Push(symbol)
+		case ")":
+			if stack.Pop() != "(" {
+				return symbol
+			}
+		case "}":
+			if stack.Pop() != "{" {
+				return symbol
+			}
+		case "]":
+			if stack.Pop() != "[" {
+				return symbol
+			}
+		case ">":
+			if stack.Pop() != "<" {
+				return symbol
+			}
+		}
+	}
+	return ""
+}
+
+func scoreIncompleteLine(l string) int {
+	var stack helpers.Stack
+	for _, c := range l {
+		symbol := string(c)
+		if symbol == "(" || symbol == "{" || symbol == "[" || symbol == "<" {
+			stack.Push(symbol)
+			continue
+		}
+		if symbol == stack.Pop() {
+			fmt.Println("correct")
+			continue
+		}
+	}
+
+	var score int
+	helpers.Reverse(stack)
+	for _, c := range stack {
+		score *= 5
+		switch string(c) {
+		case "(":
+			score += 1
+		case "{":
+			score += 3
+		case "[":
+			score += 2
+		case "<":
+			score += 4
+		}
+	}
+	return score
+}
--- helpers/convert.go
+++ helpers/convert.go
@@ -10,3 +10,9 @@ func StringSliceToIntSlice(s []string) []int {
 	}
 	return intSlice
 }
+
+func Reverse(s []string) {
+	for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
+		s[i], s[j] = s[j], s[i]
+	}
+}
