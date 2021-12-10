85b52d2e2effbe7afd2429eac69b6f315c5bedca
marc-enzo.bonnafon <marcenzo.bonnafon@gmail.com>
2021-12-10 22:35:36 +0100

    [go] simplify day 1 2021

--- 2021/day1/main.go
+++ 2021/day1/main.go
@@ -8,39 +8,27 @@ import (
 
 func main() {
 	lines, _ := helpers.StringLines("./input.txt")
-	fmt.Println("Part 1. :", pt1(lines))
-	fmt.Println("Part 2. :", pt2(lines))
+	fmt.Println("Part 1. :", pt1(helpers.ToIntSlice(lines)))
+	fmt.Println("Part 2. :", pt2(helpers.ToIntSlice(lines)))
 }
 
-func pt1(lines []string) int {
-	intLines := helpers.StringSliceToIntSlice(lines)
-	var previous, counter int
-	for k, current := range intLines {
-		if k == 0 {
-			previous = current
-			continue
+func pt1(lines []int) (counter int) {
+	for i := 1; i < len(lines); i++ {
+		if lines[i] > lines[i-1] {
+			counter++
 		}
+	}
+	return
+}
+
+func pt2(lines []int) (counter int) {
+	previous := lines[0] + lines[1] + lines[2]
+	for i := 2; i < len(lines); i++ {
+		current := lines[i] + lines[i-1] + lines[i-2]
 		if current > previous {
 			counter++
 		}
 		previous = current
 	}
-	return counter
-}
-
-func pt2(lines []string) int {
-	intLines := helpers.StringSliceToIntSlice(lines)
-	var previous, counter int
-	for i := 2; i < len(intLines); i++ {
-		current := intLines[i] + intLines[i-1] + intLines[i-2]
-		if i == 2 {
-			previous = intLines[i] + intLines[i-1] + intLines[i-2]
-			continue
-		}
-		if current > previous {
-			counter++
-		}
-		previous = current
-	}
-	return counter
+	return
 }
