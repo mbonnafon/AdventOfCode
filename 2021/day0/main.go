6dabbd04f14d6b43b73f94a407ae676ae000a3e4
marc-enzo.bonnafon <marcenzo.bonnafon@gmail.com>
2021-12-10 13:58:25 +0100

    [go] make template run

--- 2021/day0/main.go
+++ 2021/day0/main.go
@@ -6,16 +6,16 @@ import (
 	"github.com/mbonnafon/AdventOfCode/helpers"
 )
 
-func pt1(lines []string) {
-
-}
-
-func pt2(lines []string) {
-
-}
-
 func main() {
 	lines, _ := helpers.StringLines("./input.txt")
 	fmt.Println("Part 1. :", pt1(lines))
 	fmt.Println("Part 2. :", pt2(lines))
+}
+
+func pt1(lines []string) int {
+	return 0
+}
+
+func pt2(lines []string) int {
+	return 0
 }
