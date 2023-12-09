75ddae52331adbd794a903356a3fdd3d0e552be1
marc-enzo.bonnafon <marcenzo.bonnafon@gmail.com>
2023-12-09 21:48:38 +0100

    scala - 2023 day 1

--- scala/year-2023/src/main/scala/year2023/Day01.scala
+++ scala/year-2023/src/main/scala/year2023/Day01.scala
@@ -2,10 +2,56 @@ package year2023
 
 import utils.Puzzle
 
+import scala.Console.println
+import scala.util.matching.Regex
+
 object Day01 extends Puzzle {
+  def part1(input: List[String]): Int = {
+    input
+      .map(a => a.filter(_.isDigit))
+      .map { a =>
+        a.size match {
+          case 1 => a ++ a
+          case 2 => a
+          case _ => a.head.toString ++ a.last.toString
+        }
+      }
+      .map(_.toInt)
+      .sum
+  }
 
-  def part1(input: List[String]): Int = ???
+  def part2(input: List[String]): Int = {
+    val mapping = Map(
+      "one" -> "1",
+      "two" -> "2",
+      "three" -> "3",
+      "four" -> "4",
+      "five" -> "5",
+      "six" -> "6",
+      "seven" -> "7",
+      "eight" -> "8",
+      "nine" -> "9"
+    )
 
-  def part2(input: List[String]): Int = ???
+    input
+      .map { line =>
+        line.zipWithIndex
+          .map { case (c, idx) =>
+            if (c.isDigit) Some(c)
+            else {
+              mapping
+                .find { number =>
+                  line.slice(idx, line.length).startsWith(number._1)
+                }
+                .map(_._2)
+            }
+          }
+          .collect { case v: Some[Any] =>
+            v.value.toString
+          }
+      }
+      .map(line => (line.head ++ line.last).toInt)
+      .sum
+  }
 
 }
